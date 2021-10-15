abb78bd340eda3b63b753f4838fa011d96ae8e03
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-10-16 00:48:38 +0700

    CV v.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 const prefix = require("gulp-autoprefixer");
 
 function buildStyle() {
-  return src("./src/assets/scss/style.scss", { sourcemaps: true })
+  return src("./assets/scss/style.scss", { sourcemaps: true })
     .pipe(sass().on("error", sass.logError))
     .pipe(prefix("last 2 versions"))
-    .pipe(dest("src/assets/css", { sourcemaps: "." }));
+    .pipe(dest("assets/css", { sourcemaps: "." }));
 }
 
 function watchTask() {
-  watch(["./src/assets/scss/**/*.scss"], buildStyle);
+  watch(["./assets/scss/**/*.scss"], buildStyle);
 }
 
 exports.default = series(buildStyle, watchTask);
